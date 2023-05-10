d2537f1b08994511b939cf6caf0f99dd6b73aade
indexofmayank <maimayanmanu@gmail.com>
2023-05-10 14:58:17 +0530

    'Chapter3Done'

---
+++ chapter3/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+
+
+
+function App() {
+  return (
+    <div>
+      <AddPostForm />
+      <PostsList />
+    </div>
+  );
+}
+
+export default App;
